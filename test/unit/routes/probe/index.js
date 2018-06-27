03ef1996737a50d81f9a4b979a21ed2e5d11257b
Hardiarl Uppal <hardiarl.uppal@es.catapult.org.uk>
2018-06-27 19:54:34 +0100

    changed path

--- test/unit/routes/probe/index.js
+++ test/unit/routes/probe/index.js
@@ -2,7 +2,7 @@
 const { expect } = require('code');
 const Lab = require('lab');
 const { after, before, describe, it } = exports.lab = Lab.script();
-const asset = require('../../../lib/routes/probe');
+const asset = require('../../routes/probe');
 
 describe('math', () => {
 
