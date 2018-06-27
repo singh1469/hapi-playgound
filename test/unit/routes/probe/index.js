461d5c425d85b6502d92b980d462e891d54ee9cd
Hardiarl Uppal <hardiarl.uppal@es.catapult.org.uk>
2018-06-27 19:53:02 +0100

    exclude reports

--- test/unit/routes/probe/index.js
+++ test/unit/routes/probe/index.js
@@ -2,7 +2,7 @@
 const { expect } = require('code');
 const Lab = require('lab');
 const { after, before, describe, it } = exports.lab = Lab.script();
-const asset = require('../../../lib/rotes/probe');
+const asset = require('../../../lib/routes/probe');
 
 describe('math', () => {
 
