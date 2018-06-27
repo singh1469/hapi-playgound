2e7f938f9d8444a0e3fb0a81d1e63f9fdaaf2543
Hardiarl Uppal <hardiarl.uppal@es.catapult.org.uk>
2018-06-27 15:29:10 +0100

    test stuff so far

---
+++ test/lib/routes/probe/index.js
@@ -0,0 +1,16 @@
+'use-strict'
+const { expect } = require('code');
+const Lab = require('lab');
+const { after, before, describe, it } = exports.lab = Lab.script();
+const asset = require('../../../lib/rotes/probe');
+
+describe('math', () => {
+
+    before(() => {});
+
+    after(() => {});
+
+    it('returns properties name, register', () => {
+        expect(asset).to.be.an.object();
+    });
+});
